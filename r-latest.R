2a91bba5b87709bc726a29ded501f7f627f2ddc4
Lluís Revilla <lluis.revilla@gmail.com>
2024-08-09 10:11:36 +0200

    update the R version

--- r-latest.R
+++ r-latest.R
@@ -2,6 +2,6 @@ library("xml2")
 library("rtoot")
 
 # Extract data ####
-tag <- "R-4-3-branch"
+tag <- "R-4-4-branch"
 setwd("~/automated_scripts/r_dev_news/")
 source("r-core_script.R", local = TRUE, echo = TRUE)
